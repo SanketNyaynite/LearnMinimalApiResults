f4d1bc25718c8ee97b718f9244316bf4ec486954
SanketNyaynite <sanket.nyaynite@gmail.com>
2026-02-27 19:14:04 -0600

    Add new folder and organize endpoints

---
+++ WebApi/Endpoints/EmployeeEndpoints.cs
@@ -0,0 +1,46 @@
+﻿using WebApi.Models;
+using WebApi.Results;
+
+namespace WebApi.Endpoints
+{
+    public static class EmployeeEndpoints
+    {
+        public static void MapEmployeeEndpoints(this WebApplication app)
+        {
+            app.MapGet("/", HtmlResult () =>
+            {
+                string html = "<h2>Welcome to our API<h2> Our API is used to learn ASP.NET Core 9.0 features and improvements.";
+
+                return new HtmlResult(html);
+            });
+
+            app.MapGet("/employees", () =>
+            {
+                var employees = EmployeesRepository.GetEmployees();
+
+                return TypedResults.Ok(employees);
+            });
+
+            app.MapPost("/employees", (Employee employee) =>
+            {
+                if(employee is null || employee.Id < 0)
+                {
+                    return Microsoft.AspNetCore.Http.Results.ValidationProblem(new Dictionary<string, string[]>
+                //var employee = EmployeesRepository.GetEmployeeById(Id);
+                //return employee is not null
+                //? TypedResults.Ok(employee)
+                //: Microsoft.AspNetCore.Http.Results.ValidationProblem(new Dictionary<string, string[]>
+                {
+                    { "id", new[] { "Employee is not provided or is not valid." } }
+                },
+                statusCode: 400);
+                }
+
+
+                EmployeesRepository.AddEmployee(employee);
+            return TypedResults.Created($"/employees/{employee.Id}", employee);
+
+        }).WithParameterValidation();
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using WebApi.Endpoints;
 using WebApi.Models;
 using WebApi.Results;
 
@@ -14,33 +15,6 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStatusCodePages();
 
-app.MapGet("/", HtmlResult() =>
-{
-    string html = "<h2>Welcome to our API<h2> Our API is used to learn ASP.NET Core 9.0 features and improvements.";
-
-    return new HtmlResult(html);
-});
-
-app.MapGet("/employees", () =>
-{
-    var employees = EmployeesRepository.GetEmployees();
-
-    return TypedResults.Ok(employees);
-});
-
-app.MapPost("/employees", (Employee employee) =>
-{
-if (employee is null || employee.Id < 0)
-{
-    return Results.ValidationProblem(new Dictionary<string, string[]>
-    {
-        { "id", new[] { "Employee is not provided or is not valid." } }
-    });
-    }
-
-    EmployeesRepository.AddEmployee(employee);
-    return TypedResults.Created($"/employees/{employee.Id}", employee);
-
-}).WithParameterValidation();
+app.MapEmployeeEndpoints();
 
 app.Run();
